f64ebc3f7d8a4afcc5a8f5067068ee916c8b5d52
Gregory Higley <greg@prosumma.com>
2026-01-20 19:42:58 -0500

    Repo

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,21 @@ module Main (main) where
 import Lib
 import RIO
 
+data Connection = Connection
+
+class HasConnection a where
+  getConnection :: a -> Connection
+
+instance HasConnection Connection where
+  getConnection = id
+
+class UserRepository m where
+  getUserById :: Int -> m Int
+
+instance (HasConnection env) => UserRepository (RIO env) where
+  getUserById _ = do
+    conn <- asks getConnection
+    undefined
+
 main :: IO ()
 main = someFunc
