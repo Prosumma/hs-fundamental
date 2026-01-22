33dde9d91668110eb8cbdd560522e474b35574b7
Gregory Higley <greg@prosumma.com>
2026-01-22 17:54:39 -0500

    Better modules

---
+++ src/Fundamental/Operators.hs
@@ -0,0 +1,26 @@
+module Fundamental.Operators (
+    (<#>),
+    (<=>),
+) where
+
+import RIO
+import RIO.Map (singleton)
+
+-- | Shortcut to create a pair in a list
+--
+-- Useful for creating maps:
+--
+-- > Map.fromList $ "foo" <#> "bar" <> "bing" <#> "bang"
+(<#>) :: a -> b -> [(a, b)]
+a <#> b = [(a, b)]
+
+infixl 7 <#>
+
+-- | Shortcut to create a Map from a pair
+--
+-- Useful for creating maps:
+-- > "foo" <=> "bar" <> "bing" <=> "bang"
+(<=>) :: k -> v -> Map k v
+k <=> v = singleton k v
+
+infixl 7 <=>
---
+++ src/Fundamental/TH.hs
@@ -0,0 +1,37 @@
+module Fundamental.TH (
+    addL,
+    addSuffix,
+    makeLensesL,
+) where
+
+import Control.Lens hiding ((??), (.~), (.=))
+import Data.Char
+import Language.Haskell.TH
+import RIO
+
+addL :: LensRules
+addL = addSuffix "L"
+
+addSuffix ::  String -> LensRules
+addSuffix suffix = defaultFieldRules & lensField .~ suffixFieldNamer suffix
+  where
+    suffixFieldNamer :: String -> FieldNamer
+    suffixFieldNamer suffix _ _ field = maybeToList $ do
+      let fieldPart = nameBase field
+      -- e.g., LensFooL if "L" is the suffix.
+      -- This helps disambiguate if DuplicateRecordFields is
+      -- also used in the same module.
+      let cls = "Lens" ++ capitalize fieldPart ++ suffix
+      return (MethodName (mkName cls) (mkName (stripUnderscore fieldPart ++ suffix)))
+    capitalize [] = []
+    capitalize (c:cs) = toUpper c : cs
+    stripUnderscore [] = []
+    stripUnderscore s@(c:cs)
+      | c == '_' = cs
+      | otherwise = s
+
+-- | Makes lenses with an L suffix.
+--
+-- Use either this or labels.
+makeLensesL :: Name -> DecsQ
+makeLensesL = makeLensesWith addL
