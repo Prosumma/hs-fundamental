c119ee21b10429bf0da1621fbfe02030a685f7db
Gregory Higley <greg@prosumma.com>
2026-01-23 23:14:24 -0500

    Util tests (to be broken up)

--- test/Spec/Util.hs
+++ test/Spec/Util.hs
@@ -1,51 +1,51 @@
-{-# LANGUAGE FunctionalDependencies, TemplateHaskell, TypeSynonymInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module Spec.Util (testUtil) where
 
 import Control.Lens (each, _Just)
 import Formatting ((%))
-import Fundamental.Util
+import qualified Formatting as F
+import Fundamental.Formatting
+import Fundamental.Operators
+import Fundamental.TH
 import RIO
+import qualified RIO.HashMap as HashMap
 import RIO.Map (fromList)
+import qualified RIO.Set as Set
 import Test.Hspec
 
-import qualified Formatting as F
-import qualified RIO.HashMap as HashMap
-import qualified RIO.Set as Set
-
-data Something = Something {
-  x :: !Int,
-  xs :: ![Maybe Int]
-} deriving (Eq, Show)
+data Something = Something
+  { x :: !Int,
+    xs :: ![Maybe Int]
+  }
+  deriving (Eq, Show)
 
 makeLensesL ''Something
 
 testUtil :: Spec
 testUtil = do
-  describe "<->" $
-    it "stitches together a pair" $
-      2 <-> 3 `shouldBe` (2, 3)
-  describe "<#>" $
-    it "stitches together a pair into a list" $ do
+  describe "<#>"
+    $ it "stitches together a pair into a list"
+    $ do
       let expected = fromList [(2, 3), (4, 5)]
       let tested = fromList $ 2 <#> 3 <> 4 <#> 5
       expected `shouldBe` tested
-  describe "<=>" $
-    it "stitches together a pair into a Map" $ do
+  describe "<=>"
+    $ it "stitches together a pair into a Map"
+    $ do
       let expected = fromList [(2, 3), (4, 5)]
       let tested = 2 <=> 3 <> 4 <=> 5
       expected `shouldBe` tested
   describe "makeLensesL" $ do
     it "makes lenses with the suffix L" $ do
       let something = Something 2 [Just 4]
-      something^.xL `shouldBe` 2
-      something^..xsL.each._Just `shouldBe` [4]
-  describe "uformat" $
-    it "formats a RIO Utf8Builder" $ do
+      something ^. xL `shouldBe` 2
+      something ^.. xsL . each . _Just `shouldBe` [4]
+  describe "uformat"
+    $ it "formats a RIO Utf8Builder"
+    $ do
       let u = uformat ("The value of " % F.text % " is " % F.int % ".") "three" 3
       utf8BuilderToText u `shouldBe` "The value of three is 3."
-  describe "extractKeys" $ do
-    it "extracts specific keys from a HashMap" $ do
-      let hm :: HashMap Text Text = HashMap.fromList ["foo" <-> "bar", "bar" <-> "baz", "biz" <-> "bee"]
-      let ex = extractKeys (Set.fromList ["foo", "bar"]) hm
-      ex `shouldBe` HashMap.fromList ["foo" <-> "bar", "bar" <-> "baz"]
