7decd21d8be7388151cb8cc3ecf03ab9b647cfa5
Gregory Higley <greg@prosumma.com>
2026-07-02 21:34:14 -0400

    Fundamental Tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,17 @@
 import RIO
+import Spec.Control
+import Spec.Exceptions
+import Spec.Pool
+import Spec.Servant
+import Spec.Textual
 import Spec.Util
 import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
   testUtil
+  testControl
+  testExceptions
+  testTextual
+  testServant
+  testPool
